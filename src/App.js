0b3f692f7c7f08c738803df6945e2367bdb0bf31
jjijju <cheytac406@gmail.com>
2021-01-15 11:43:26 +0900

    Fix: AND 연산자(&&) 조건부 랜더링
    - &&를 통해 더 짧은 코드 작업 가능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function App() {
-    const name = '리액트';
+    const name = '뤼액트';
 
-    return <div>{name === '리액트' ? <h1>리액트 입니다.</h1> : <h2>리액트가 아닙니다.</h2>}</div>;
+    return <div>{name === '리액트' && <h1>리액트 입니다.</h1>}</div>;
 }
 
 // <Fragment></Fragment> 대신 <></> 사용 가능
