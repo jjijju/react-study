51c2260cffbb995adb1c6ccaaca3356808a4b7ab
jjijju <cheytac406@gmail.com>
2021-01-15 12:10:02 +0900

    주석
    - jsx에서의 주석은 일반 자바스크립트 작성과는 다르다

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ function App() {
 
     return (
         <>
-            <div className="react">{name}</div>
-            <input></input>
+            {/* 주석은 이렇게 작성 */}
+            <div
+                className="react"
+                // 시작 태그를 여러 줄로 작성 시에는 여기에도 주석 작성 가능
+            >
+                {name}
+            </div>
+            // 이런 주석이나 /* 이런 주석은 페이지에 그대로 나타남 */
         </>
-        // self-closing 태그 <input />
     );
 }
 
