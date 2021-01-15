460657d128795e7ae8f68bba4295ca3f0947440a
jjijju <cheytac406@gmail.com>
2021-01-15 11:52:55 +0900

    인라인 스타일링
    - 스타일 표기의 - 가 사라지고 카멜 표기법으로 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 function App() {
-    const name = undefined;
-    return <div>{name || '리액트'}</div>;
-}
+    const name = '리액트';
+    const style = {
+        // 스타일 이름의 -가 사라지고 카멜 표기법으로 작성
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16,
+    };
 
-// jsx 내부에서 undefined를 렌더링하는 것은 ok
-// return <div>{name}</div>
+    return <div style={style}>{name}</div>;
+
+    // 스타일을 미리 선언하지 않고
+    // 바로 적용하고 싶으면 div의 style에서 직접 선언
+}
 
 export default App;
