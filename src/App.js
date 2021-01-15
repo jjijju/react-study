0021e6924178e9feb29f1348712c87582e1a809a
jjijju <cheytac406@gmail.com>
2021-01-15 11:58:03 +0900

    class 대신 className
    - class 대신 className 사용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
+import './App.css';
 
 function App() {
     const name = '리액트';
-    const style = {
-        // 스타일 이름의 -가 사라지고 카멜 표기법으로 작성
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 16,
-    };
 
-    return <div style={style}>{name}</div>;
-
-    // 스타일을 미리 선언하지 않고
-    // 바로 적용하고 싶으면 div의 style에서 직접 선언
+    return <div className="react">{name}</div>;
 }
 
 export default App;
