ece8e57539e85ec775bec3a8f3e301de9425d345
jjijju <cheytac406@gmail.com>
2021-01-15 11:33:39 +0900

    Fix: 자바스크립트 표현
    - jsx 문법안에서도 자바스크립트가 표현이 가능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 
 function App() {
+    const name = '리액트';
+
     return (
         <Fragment>
-            <h1>안녕 리액트</h1>
+            <h1>안녕 {name}</h1>
             <h2>정상 작동?</h2>
         </Fragment>
     );
