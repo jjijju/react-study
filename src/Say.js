130320e08fb3897f9f4f70cd923eee643a9a2c36
jjijju <cheytac406@gmail.com>
2021-01-19 16:14:46 +0900

    useState 여러번 사용하기

--- src/Say.js
+++ src/Say.js
@@ -5,11 +5,22 @@ const Say = () => {
 	const onclickEnter = () => setMessage('안녕하세요');
 	const onclickLeave = () => setMessage('안녕히 가세요');
 
+	const [color, setColor] = useState('black');
+
 	return (
 		<div>
 			<button onClick={onclickEnter}>입장</button>
 			<button onClick={onclickLeave}>퇴장</button>
-			<h1>{message}</h1>
+			<h1 style={{ color }}>{message}</h1>
+			<button style={{ color: 'red' }} onClick={() => setColor('red')}>
+				빨간색
+			</button>
+			<button style={{ color: 'green' }} onClick={() => setColor('green')}>
+				초록색
+			</button>
+			<button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+				파란색
+			</button>
 		</div>
 	);
 };
