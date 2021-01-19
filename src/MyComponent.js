bdd80f69114d401ef39852aa8bc4e13be3ce5dcf
jjijju <cheytac406@gmail.com>
2021-01-19 15:05:22 +0900

    propTypes를 이용한 검증

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
 	return (
 		<div>
 			새로운 컴포넌트 이름은 {name} <br />
-			children 값은 {children}
+			children 값은 {children} <br />
+			좋아하는 숫자는 {favoriteNumber}
 		</div>
 	);
 };
 
 MyComponent.defaultProps = {
 	name: 'default name',
+};
+
+MyComponent.propTypes = {
+	name: PropTypes.string,
+	favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
