6da4e432aacfdccace3db0ee234072c9a1d9316c
jjijju <cheytac406@gmail.com>
2021-01-19 14:31:11 +0900

    컴포넌트
    - 컴포넌트 생성 및 내보내기&불러오기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import MyComponent from './MyComponent';
 
-function App() {
-    const name = '리액트';
-
-    return (
-        <>
-            {/* 주석은 이렇게 작성 */}
-            <div
-                className="react"
-                // 시작 태그를 여러 줄로 작성 시에는 여기에도 주석 작성 가능
-            >
-                {name}
-            </div>
-            // 이런 주석이나 /* 이런 주석은 페이지에 그대로 나타남 */
-        </>
-    );
-}
+const App = () => {
+	return <MyComponent />;
+};
 
 export default App;
