9940c77fe1f19a3d3c7b113ff4079ee86e452517
jjijju <cheytac406@gmail.com>
2021-01-25 15:22:52 +0900

    이벤트 input 여러개 다루기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+	state = {
+		username: '',
+		message: '',
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	handleClick = () => {
+		alert(this.state.username + ': ' + this.state.message);
+		this.setState({
+			username: '',
+			message: '',
+		});
+	};
+
 	render() {
 		return (
 			<div>
 				<h1>이벤트 연습</h1>
 				<input
 					type="text"
-					name="message"
+					name="username"
 					placeholder="아무말"
-					onChange={(e) => {
-						console.log(e.target.value);
-					}}
+					value={this.state.username}
+					onChange={this.handleChange}
 				/>
+				<input
+					type="text"
+					name="message"
+					placeholder="아무 내용"
+					value={this.state.message}
+					onChange={this.handleChange}
+				/>
+				<button onClick={this.handleClick}>확인</button>
 			</div>
 		);
 	}
