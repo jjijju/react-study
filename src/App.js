828b5654546d590a60e129ceb8cb01e40a3edc5c
jjijju <cheytac406@gmail.com>
2021-01-27 16:14:30 +0900

    map 함수

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-// import MyComponent from './MyComponent';
-// import Counter from './Counter';
-// import Say from './Say';
-// import EventPractice from './EventPractice';
-// import ValidationSample from './ValidationSample';
+import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
+import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
+import InterationSample from './InterationSample';
 class App extends Component {
 	render() {
 		return (
 			<div>
+				<MyComponent />
+				<br />
+				<Counter />
+				<br />
+				<Say />
+				<br />
+				<EventPractice />
+				<br />
+				<ValidationSample />
+				<br />
 				<ScrollBox ref={(ref) => (this.scrollBox = ref)} />
 				<button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+				<br />
+				<InterationSample />
 			</div>
 		);
 	}
