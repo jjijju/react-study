df69b1c453d53e8ede80835244bae1cd7750639b
jjijju <cheytac406@gmail.com>
2021-01-27 16:37:13 +0900

    데이터 추가 기능

--- src/InterationSample.js
+++ src/InterationSample.js
@@ -10,9 +10,26 @@ const InterationSample = () => {
 	const [inputText, setInputText] = useState('');
 	const [nextId, setNextId] = useState(5);
 
+	const onChange = (e) => setInputText(e.target.value);
+	const onClick = () => {
+		const nextNames = names.concat({
+			id: nextId,
+			text: inputText,
+		});
+		setNextId(nextId + 1);
+		setNames(nextNames);
+		setInputText('');
+	};
+
 	const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
 
-	return <ul>{nameList}</ul>;
+	return (
+		<>
+			<input value={inputText} onChange={onChange} />
+			<button onClick={onClick}>추가</button>
+			<ul>{nameList}</ul>
+		</>
+	);
 };
 
 export default InterationSample;
