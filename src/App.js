9d2978672bc84866328d7ab0f4bd16ea47c65389
jjijju <cheytac406@gmail.com>
2021-02-01 14:45:34 +0900

    라이프 사이클
    - 메서드 사용하고 살펴보기

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent';
-import Counter from './Counter';
-import Say from './Say';
-import EventPractice from './EventPractice';
-import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
-import InterationSample from './InterationSample';
 
 // import Jsx from './02_jsx';
 // import Components from './03_component';
+import LifeCycle from './07_life_cycle';
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<MyComponent name="React" favoriteNumber={9} />
-				<br />
-				<Counter />
-				<br />
-				<Say />
-				<br />
-				<EventPractice />
-				<br />
-				<ValidationSample />
-				<br />
-				<ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-				<button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
-				<br />
-				<br />
-				<InterationSample />
+				<LifeCycle />
 			</div>
 		);
 	}
