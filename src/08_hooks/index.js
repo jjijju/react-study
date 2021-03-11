eedd3a10b49b036189f339f62778541f9e596624
jjijju <cheytac406@gmail.com>
2021-03-11 16:20:43 +0900

    Hooks 기능

--- src/08_hooks/index.js
+++ src/08_hooks/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Ex01 from './ex01';
 import Ex02 from './ex02';
 import Ex03 from './ex03';
+import Ex04 from './ex04';
+import Ex05 from './ex05';
+import Ex06 from './ex06';
 
 const Index = () => {
 	return (
@@ -10,6 +13,9 @@ const Index = () => {
 			<Ex01 />
 			<Ex02 />
 			<Ex03 />
+			<Ex04 />
+			<Ex05 />
+			<Ex06 />
 		</>
 	);
 };
